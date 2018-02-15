ad699a33c4c248a62a0e5896f627cc0776e4d834
Derek Roberts <ratchet315@gmail.com>
2018-02-14 21:26:37 -0800

    Initial Commit

---
+++ Securis/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Securis
+//
+//  Created by Derek Roberts on 2/14/18.
+//  Copyright © 2018 Derek Roberts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
