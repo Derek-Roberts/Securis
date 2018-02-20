6dcd47e177d585bcfdf3c9635a963c9cfe10142a
Derek Roberts <ratchet315@gmail.com>
2018-02-20 14:02:06 -0800

    Added custom 'slide' segue transition and added back buttons to the sign in and sign up views for testing purposes.

---
+++ Securis/SlideSegue.swift
@@ -0,0 +1,49 @@
+//
+//  SlideSegue.swift
+//  Securis
+//
+//  Created by Derek Roberts on 2/20/18.
+//  Copyright © 2018 Derek Roberts. All rights reserved.
+//
+
+import UIKit
+
+class SlideSegue: UIStoryboardSegue {
+    override func perform() {
+        slideIn()
+    }
+    
+    func slideIn() {
+        let toViewController = self.destination
+        let fromViewController = self.source
+        let containerView = fromViewController.view.superview
+
+        toViewController.view.transform = CGAffineTransform(translationX: 512, y: 0)
+        containerView?.addSubview(toViewController.view)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+            toViewController.view.transform = CGAffineTransform.identity
+        }, completion: { success in
+            fromViewController.present(toViewController, animated: false, completion: nil)
+        })
+    }
+}
+
+class UnwindSlideSegue: UIStoryboardSegue {
+    override func perform() {
+        slideIn()
+    }
+    
+    func slideIn() {
+        let toViewController = self.destination
+        let fromViewController = self.source
+        
+        fromViewController.view.superview?.insertSubview(toViewController.view, at: 0)
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut, animations: {
+            fromViewController.view.transform = CGAffineTransform(translationX: 512, y: 0)
+        }, completion: { success in
+            fromViewController.dismiss(animated: false, completion: nil)
+        })
+    }
+}
--- Securis/ViewController.swift
+++ Securis/ViewController.swift
@@ -14,6 +14,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func prepareForUnwind (segue:UIStoryboardSegue) {
+        
+    }
+    
+    override func unwind(for unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController) {
+        let segue = UnwindSlideSegue(identifier: unwindSegue.identifier, source: unwindSegue.source, destination: unwindSegue.destination)
+        segue.perform()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
